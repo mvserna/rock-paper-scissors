f12709a14284d41ac06094ae7bb523e0d8eb996b
Martin Villarreal Serna <martin.serna@gmail.com>
2021-05-29 21:26:50 -0400

    Add pseudocode

---
+++ main.js
@@ -0,0 +1,18 @@
+// Ask the user for a selection
+  // Prompt user to make a choice
+  // Assign user's choice to a variable
+  // Call a function to parse the variable, which will
+    // Convert variable to lower case
+    // If it is 'rock', 'paper', or 'scissor' return the variable
+      // Else run error message, "That's not a choice!" and return to prompt.
+// Have the computer make a random selection
+  // Call a function, which will:
+    // Choose a random number between 1 and 3
+    // Assign that random number to a variable
+    // Converts the random number to the appropriate response
+      // If the variable is 1 then 'rock'
+      // If the variable is 2 then 'paper'
+      // If the variable is 3 then 'scissors'
+    // Return the correct string
+// Determine the winner.
+// Output whether the user won or lost.
